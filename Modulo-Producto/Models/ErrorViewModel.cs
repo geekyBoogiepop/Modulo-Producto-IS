b1b9fa32ac8bd46f36724082403eb76169112cf8
geekyBoogiepop <fljustsocial@outlook.com>
2023-06-27 16:12:03 -0600

    Agregar archivos de proyecto.

---
+++ Modulo-Producto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Modulo_Producto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
