e2596642ae285c31b2fcb0650f800388cbe37178
geekyBoogiepop <fljustsocial@outlook.com>
2023-06-27 21:06:27 -0600

    feat: agregar estilos básicos a las vistas de Productos

--- Modulo-Producto/Models/Producto.cs
+++ Modulo-Producto/Models/Producto.cs
@@ -5,26 +5,32 @@ namespace Modulo_Producto.Models
     {
         public int Id { get; set; }
 
+        [Required]
         [Display(Name = "Codigo de Barra")]
         public string? CodigoBarra { get; set; }
 
+        [Required]
         public string? Descripcion { get; set; }
 
-        [Display(Name = "Precio de Costo")]
+        [Required]
+        [Display(Name = "Precio de Costo (C$)")]
         public float? PrecioCosto { get; set; }
 
-        [Display(Name = "Precio de Mayoreo")]
+        [Required]
+        [Display(Name = "Precio de Mayoreo (C$)")]
         public float? PrecioMayoreo { get; set; }
 
-        [Display(Name = "Precio de Venta")]
+        [Required]
+        [Display(Name = "Precio de Venta (C$)")]
         public float? PrecioVenta { get; set; }
 
+        [Required]
         public float? Impuestos { get; set; }
 
-        [Display(Name = "Tiene Inventario")]
+        [Display(Name = "Tiene Inventario (C$)")]
         public bool TieneInventario { get; set; }
 
-        [Display(Name = "Cantidad actual")]
+        [Display(Name = "Cantidad actual (C$)")]
         public int? CantidadActual { get; set; }
 
         public int? Minimo { get; set; }
